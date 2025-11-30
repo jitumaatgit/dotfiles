fd5255303ff4af1a5db70efcff579c20d5698df8
CALI117u47 <CALI117u47@ad.pcc.edu>
2025-11-30 10:55:35 -0800

    fix wezterm opacity and make more cattpucin like

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,17 +1,61 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
+-- make git bash the default shell (via Scoop)
+config.default_prog = {
+	"C:/Users/student/scoop/apps/git/current/bin/bash.exe",
+	"--login",
+	"-i",
+}
 -- Basics (Kitty/Tmux-like)
-config.font = wezterm.font("JetBrains Mono") -- Fixme (figure out how to get wezterm to find installed nerd font)
-config.color_scheme = "Tokyo Night" -- change to cattpuccin mocha
+config.font = wezterm.font_with_fallback({
+	"JetBrains Mono Nerd Font",
+	"JetBrains Mono",
+})
+config.font_size = 12.0
+config.color_scheme = "Cattpuccin Mocha" -- change to cattpuccin mocha
 config.window_padding = { left = 5, right = 5, top = 5, bottom = 5 }
 config.initial_cols = 120
 config.initial_rows = 40
-config.window_background_opacity = 0.95
 config.tab_bar_at_bottom = false
 config.use_fancy_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = false
+config.tab_max_width = 32
+config.colors = {
+	tab_bar = {
+		background = "#1e1e2e",
+
+		active_tab = {
+			bg_color = "#585b70",
+			fg_color = "#f5e0dc",
+			intensity = "Bold",
+		},
+
+		inactive_tab = {
+			bg_color = "#181825",
+			fg_color = "#a6adc8",
+		},
+
+		inactive_tab_hover = {
+			bg_color = "#313244",
+			fg_color = "#cdd6f4",
+			italic = true,
+		},
+
+		new_tab = {
+			bg_color = "#1e1e2e",
+			fg_color = "#a6adc8",
+		},
+
+		new_tab_hover = {
+			bg_color = "#313244",
+			fg_color = "#cdd6f4",
+			italic = true,
+		},
+	},
+}
 config.audible_bell = "Disabled"
 -- Leader key: Ctrl-b (tmux style)
-config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 1000 }
+config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 2000 }
 -- Main key assignments
 config.keys = {
 	-- Tabs (tmux: Ctrl-b c/n/p/&/w)
