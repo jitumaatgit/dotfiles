b443d2c9be88643b5f6987d5448eb8c66a516036
Jitu <jitumaat@protonmail.com>
2025-12-12 19:20:31 -0800

    add jk and kk to esc keymap

--- AppData/Local/nvim/lua/config/keymaps.lua
+++ AppData/Local/nvim/lua/config/keymaps.lua
@@ -2,3 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("i", "kk", "<Esc>")
+vim.keymap.set("i", "jk", "<Esc>")
