c4e4d1a73103fc7ed02855136ee0f7480bdaeaf3
Jitu <jitumaat@protonmail.com>
2025-12-29 18:24:44 -0800

    add sqlite3 path to options.lua

--- AppData/Local/nvim/lua/config/options.lua
+++ AppData/Local/nvim/lua/config/options.lua
@@ -2,3 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.scrolloff = 8 -- line buffer above and below
+
+-- SQLite DLL path for yanky.nvim (sqlite.lua)
+vim.g.sqlite_clib_path = "C:/Users/student/.local/bin/sqlite3.dll"
