be84a22be56c98269f0c0c0fc85ca99fb3ebdb41
Jitu <jitumaat@protonmail.com>
2026-01-28 12:57:59 -0800

    fix snacks terminal in neovim not having paths set for shims by explicitly providing opencode path

--- AppData/Local/nvim/lua/plugins/opencode.lua
+++ AppData/Local/nvim/lua/plugins/opencode.lua
@@ -11,27 +11,29 @@ return {
       ---@type opencode.Opts
       vim.g.opencode_opts = {
         -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition".
-        port = 3000,
+        -- port = 3000, dont enable this, opencode.nvim will not find the process if I set port manually (on my windows
+        -- setup)
         provider = {
-          enabled = "wezterm",
-          wezterm = {
-            direction = "bottom",
-            top_level = false,
-            percent = 40,
-          },
-          -- snacks = {
-          --   auto_close = true,
-          --   win = {
-          --     position = "right",
-          --     enter = false,
-          --     wo = {
-          --       winbar = "",
-          --     },
-          --     bo = {
-          --       filetype = "opencode_terminal",
-          --     },
-          --   },
+          cmd = "C:/Users/student/scoop/shims/opencode.exe"
+          enabled = "snacks",
+          -- wezterm = {
+          --   direction = "bottom",
+          --   top_level = false,
+          --   percent = 40,
           -- },
+          snacks = {
+            auto_close = true,
+            win = {
+              position = "right",
+              enter = false,
+              wo = {
+                winbar = "",
+              },
+              bo = {
+                filetype = "opencode_terminal",
+              },
+            },
+          },
         },
       }
 
