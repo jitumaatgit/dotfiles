09e4366e60a8d2d45b2cc6420af4ba0e17ef6ab9
Jitu <jitumaat@protonmail.com>
2026-01-30 14:59:57 -0800

    fix

--- portable-dev/autohotkey-portable/remap-v2.ahk
+++ portable-dev/autohotkey-portable/remap-v2.ahk
@@ -7,25 +7,73 @@ ProcessSetPriority "H"
 SetWinDelay -1
 SetControlDelay -1
 
+; Include VD.ah2 library
 #Include %A_ScriptDir%\VD.ah2
 
+; Configure VD
 VD.animation_on:=false
 VD.createUntil(5)
 
-; Desktop switch
-Loop 9 { hotkey "#" A_Index, ((i) => VD.goToDesktopNum(i)).Bind(A_Index) }
+; Desktop switch: Win + 1-9
+#1::GoToDesktop(1)
+#2::GoToDesktop(2)
+#3::GoToDesktop(3)
+#4::GoToDesktop(4)
+#5::GoToDesktop(5)
+#6::GoToDesktop(6)
+#7::GoToDesktop(7)
+#8::GoToDesktop(8)
+#9::GoToDesktop(9)
+
+GoToDesktop(num) {
+    try {
+        VD.goToDesktopNum(num)
+    } catch {
+        ; Fallback: use Windows default
+        Send "#{Tab}"
+    }
+}
 
 ; Desktop nav
 #[::VD.goToRelativeDesktopNum(-1)
 #]::VD.goToRelativeDesktopNum(1)
 
-; Move window + follow
-Loop 9 { hotkey "#+" A_Index, ((i) => VD.MoveWindowToDesktopNum("A",i).follow()).Bind(A_Index) }
+; Move window + follow: Win + Shift + 1-9
+#+1::MoveWindowToDesktop(1, true)
+#+2::MoveWindowToDesktop(2, true)
+#+3::MoveWindowToDesktop(3, true)
+#+4::MoveWindowToDesktop(4, true)
+#+5::MoveWindowToDesktop(5, true)
+#+6::MoveWindowToDesktop(6, true)
+#+7::MoveWindowToDesktop(7, true)
+#+8::MoveWindowToDesktop(8, true)
+#+9::MoveWindowToDesktop(9, true)
 
-; Move window - stay
-Loop 9 { hotkey "#!" A_Index, ((i) => VD.MoveWindowToDesktopNum("A",i)).Bind(A_Index) }
+MoveWindowToDesktop(num, follow := false) {
+    try {
+        if (follow) {
+            VD.MoveWindowToDesktopNum("A", num).follow()
+        } else {
+            VD.MoveWindowToDesktopNum("A", num)
+        }
+    } catch {
+        ; Fallback
+        Send "#{Tab}"
+    }
+}
 
-; Pin window
+; Move window - stay: Win + Alt + 1-9
+#!1::MoveWindowToDesktop(1, false)
+#!2::MoveWindowToDesktop(2, false)
+#!3::MoveWindowToDesktop(3, false)
+#!4::MoveWindowToDesktop(4, false)
+#!5::MoveWindowToDesktop(5, false)
+#!6::MoveWindowToDesktop(6, false)
+#!7::MoveWindowToDesktop(7, false)
+#!8::MoveWindowToDesktop(8, false)
+#!9::MoveWindowToDesktop(9, false)
+
+; Pin window: Win + Shift + P
 #+p::VD.TogglePinWindow("A")
 
 ; Remaps
--- setup.ps1
+++ setup.ps1
@@ -249,43 +249,71 @@ ProcessSetPriority "H"
 SetWinDelay -1
 SetControlDelay -1
 
+; Include VD.ah2 library
 #Include %A_ScriptDir%\VD.ah2
 
+; Configure VD
 VD.animation_on:=false
 VD.createUntil(5)
 
 ; Desktop switch: Win + 1-9
-Loop 9 {
-    Hotkey "#" . A_Index, "SwitchDesktop"
+#1::GoToDesktop(1)
+#2::GoToDesktop(2)
+#3::GoToDesktop(3)
+#4::GoToDesktop(4)
+#5::GoToDesktop(5)
+#6::GoToDesktop(6)
+#7::GoToDesktop(7)
+#8::GoToDesktop(8)
+#9::GoToDesktop(9)
+
+GoToDesktop(num) {
+    try {
+        VD.goToDesktopNum(num)
+    } catch {
+        ; Fallback: use Windows default
+        Send "#{Tab}"
+    }
 }
 
-SwitchDesktop(ThisHotkey) {
-    VD.goToDesktopNum(SubStr(ThisHotkey.Name, -1))
-}
-
-; Desktop nav: Win + [ / ]
+; Desktop nav
 #[::VD.goToRelativeDesktopNum(-1)
 #]::VD.goToRelativeDesktopNum(1)
 
 ; Move window + follow: Win + Shift + 1-9
-Loop 9 {
-    Hotkey "#+" . A_Index, "MoveWindowFollow"
-}
+#+1::MoveWindowToDesktop(1, true)
+#+2::MoveWindowToDesktop(2, true)
+#+3::MoveWindowToDesktop(3, true)
+#+4::MoveWindowToDesktop(4, true)
+#+5::MoveWindowToDesktop(5, true)
+#+6::MoveWindowToDesktop(6, true)
+#+7::MoveWindowToDesktop(7, true)
+#+8::MoveWindowToDesktop(8, true)
+#+9::MoveWindowToDesktop(9, true)
 
-MoveWindowFollow(ThisHotkey) {
-    desktopNum := SubStr(ThisHotkey.Name, -1)
-    VD.MoveWindowToDesktopNum("A", desktopNum).follow()
+MoveWindowToDesktop(num, follow := false) {
+    try {
+        if (follow) {
+            VD.MoveWindowToDesktopNum("A", num).follow()
+        } else {
+            VD.MoveWindowToDesktopNum("A", num)
+        }
+    } catch {
+        ; Fallback
+        Send "#{Tab}"
+    }
 }
 
 ; Move window - stay: Win + Alt + 1-9
-Loop 9 {
-    Hotkey "#!" . A_Index, "MoveWindowStay"
-}
-
-MoveWindowStay(ThisHotkey) {
-    desktopNum := SubStr(ThisHotkey.Name, -1)
-    VD.MoveWindowToDesktopNum("A", desktopNum)
-}
+#!1::MoveWindowToDesktop(1, false)
+#!2::MoveWindowToDesktop(2, false)
+#!3::MoveWindowToDesktop(3, false)
+#!4::MoveWindowToDesktop(4, false)
+#!5::MoveWindowToDesktop(5, false)
+#!6::MoveWindowToDesktop(6, false)
+#!7::MoveWindowToDesktop(7, false)
+#!8::MoveWindowToDesktop(8, false)
+#!9::MoveWindowToDesktop(9, false)
 
 ; Pin window: Win + Shift + P
 #+p::VD.TogglePinWindow("A")
