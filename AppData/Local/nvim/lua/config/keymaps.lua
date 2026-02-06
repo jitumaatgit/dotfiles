7d6de492cceff6eeabbc64c8ff12bfe31cd5b426
Jitu <jitumaat@protonmail.com>
2026-02-06 14:41:48 -0800

    add some smart-dd & cool undo-breakpoint keymaps & add yazi to setup script

--- AppData/Local/nvim/lua/config/keymaps.lua
+++ AppData/Local/nvim/lua/config/keymaps.lua
@@ -1,11 +1,34 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- shorthand for mapping keys
+local map = vim.keymap.set
+
 vim.keymap.set("i", "jj", "<Esc>")
 vim.keymap.set("i", "kk", "<Esc>")
 vim.keymap.set("i", "jk", "<Esc>")
 vim.keymap.set("i", "kj", "<Esc>")
 vim.keymap.set("n", "gj", [[/^#\+ .*<CR>]], { desc = "Next markdown heading" })
 vim.keymap.set("n", "gk", [[?^#\+ .*<CR>]], { desc = "Previous markdown heading" })
+
 -- Pressing Esc clears search highlight
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<cr>")
+
+-- Add undo break-points
+map("i", ",", ",<c-g>u")
+map("i", ".", ".<c-g>u")
+map("i", ";", ";<c-g>u")
+
+-- smart deletion, dd
+-- It solves the issue, where you want to delete empty line, but dd will override you last yank.
+-- Code above will check if u are deleting empty line, if so - use black hole register.
+-- [src: https://www.reddit.com/r/neovim/comments/w0jzzv/comment/igfjx5y/?utm_source=share&utm_medium=web2x&context=3]
+local function smart_dd()
+  if vim.api.nvim_get_current_line():match("^%s*$") then
+    return '"_dd'
+  else
+    return "dd"
+  end
+end
+
+vim.keymap.set("n", "dd", smart_dd, { noremap = true, expr = true })
