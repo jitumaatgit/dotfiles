86c8029c2fe85b36c827f72593161769048e7543
Jitu <jitumaat@protonmail.com>
2026-03-21 14:10:49 -0700

    add quick-scope plugin and flash config for FftT jumps

---
+++ AppData/Local/nvim/lua/plugins/extend-flash.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/flash.nvim",
+  -- commenting out disabling flash char jump so i can use it with always on quick-scope
+  -- opts = {
+  --   modes = {
+  --     char = {
+  --       enabled = false,
+  --     },
+  --   },
+  -- },
+}
